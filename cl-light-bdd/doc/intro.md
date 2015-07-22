5d1c03f24c19b7e19337a13161a0b8f61a9355ee
Alexander <bortnikab@gmail.com>
2015-07-22 17:46:51 +0100

    Create project.

---
+++ cl-light-bdd/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cl-light-bdd
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ cl-light-bdd/test/cl_light_bdd/core_test.clj
@@ -0,0 +1,7 @@
+(ns cl-light-bdd.core-test
+  (:require [clojure.test :refer :all]
+            [cl-light-bdd.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
